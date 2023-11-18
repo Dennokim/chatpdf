d1ee6e6ef480bbb00d1c9ae455dfc7b2486c02b2
Denis <deniskimani2022@gmail.com>
2023-11-18 16:29:06 +0300

    have connected to pinecone db and created the index

---
+++ chatpdf/src/lib/pinecone-client.ts
@@ -0,0 +1,29 @@
+import { Pinecone } from "@pinecone-database/pinecone";
+import {env} from './config';
+
+async function initializeAndCreateIndex() {
+  // Initialize the Pinecone client
+  const pinecone = new Pinecone({
+    apiKey: process.env.PINECONE_API_KEY!,
+    environment: process.env.PINECONE_ENVIRONMENT!,
+  });
+
+  // Create the index
+  try {
+    await pinecone.createIndex({
+      name: "chatpdf",
+      dimension: 1536,
+      metric: "cosine",
+    });
+    console.log(
+        `Waiting for ${env.INDEX_INIT_TIMEOUT} seconds for index initialization to complete...`
+      );
+      setTimeout(() => {}, env.INDEX_INIT_TIMEOUT * 1000);
+  }
+  catch (err) {
+    console.log("Error creating index");
+  }
+}
+
+// Export the function
+export { initializeAndCreateIndex };
